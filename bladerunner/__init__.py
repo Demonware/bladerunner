6585a17b1b5ec083b8b114dad3d3eee10b80d816
a-tal <github@talsma.ca>
2014-05-15 18:20:45 -0700

    4.0.6

--- bladerunner/__init__.py
+++ bladerunner/__init__.py
@@ -30,8 +30,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 
-__version__ = "4.0.5"
-__release_date__ = "April 23, 2014"
+__version__ = "4.0.6"
+__release_date__ = "May 15, 2014"
 
 
 from bladerunner.base import Bladerunner
