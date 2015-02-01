b47fb84b0fd01516566e56479a6628250f6463d2
Berker Peksag <berker.peksag@gmail.com>
2015-02-01 06:22:48 +0200

    Add trove classifiers for Python versions.

--- setup.py
+++ setup.py
@@ -66,5 +66,11 @@ setup(
         "Programming Language :: Python",
         "Topic :: System :: Clustering",
         "Topic :: System :: Systems Administration",
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
 )
